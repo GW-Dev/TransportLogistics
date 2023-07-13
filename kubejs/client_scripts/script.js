3330c2d7dacbf701cc03e1be180e266b8536ab83
GW-Dev <grantewilliams51@gmail.com>
2023-07-13 14:01:26 -0500

    Gravestone, Map, and Netherrack Update

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -6,6 +6,4 @@ JEIEvents.hideItems(event => {
 	// Hide items in JEI here
 	// event.hide('minecraft:cobblestone')
 
-	event.hide(	'delightfulcreators:purulent_tea')
-
 })
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -4,6 +4,7 @@ console.info('Hello, World! (You will see this line every time server resources 
 
 ServerEvents.recipes(event => {
 	// Change recipes here
+	event.recipes.create.compacting('minecraft:netherrack', ['create:cinder_flour', 'minecraft:flint', Fluid.of('minecraft:lava', 2100)])
 })
 
 ServerEvents.tags('item', event => {
@@ -13,40 +14,5 @@ ServerEvents.tags('item', event => {
 	// add the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.add('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 
-	'delightfulcreators:purulent_tea'
-	'delightfulcreators:dandelion_tea'
-	'delightfulcreators:rose_hip_tea'
-	'delightfulcreators:black_tea'
-	'delightfulcreators:yellow_tea'
-	'delightfulcreators:green_tea'
-	'delightfulcreators:coffee'
-	'delightfulcreators:long_dandelion_tea'
-	'delightfulcreators:long_black_tea'
-	'delightfulcreators:long_green_tea'
-	'delightfulcreators:long_coffee'
-	'delightfulcreators:strong_dandelion_tea'
-	'delightfulcreators:strong_black_tea'
-	'delightfulcreators:strong_green_tea'
-	'delightfulcreators:strong_coffee'
-	'delightfulcreators:strong_purulent_tea'
-	'delightfulcreators:strong_rose_hip_tea'
-	'delightfulcreators:strong_yellow_tea'
-	'delightfulcreators:rose_hip_jam'
-	'delightfulcreators:blazing_chili'
-	'delightfulcreators:tea_curry'
-	'delightfulcreators:coffe_cake_coating'
-	'delightfulcreators:coffee_cake_sponge'
-	'delightfulcreators:incomplete_coffee_cake'
-	'delightfulcreators:incomplete_rose_hip_pie'
-	'delightfulcreators:incomplete_black_cod'
-	'delightfulcreators:purulent_tea_bucket'
-	'delightfulcreators:dandelion_tea_bucket'
-	'delightfulcreators:rose_hip_tea_bucket'
-	'delightfulcreators:black_tea_bucket'
-	'delightfulcreators:yellow_tea_bucket'
-	'delightfulcreators:green_tea_bucket'
-	'delightfulcreators:coffee_bucket'
-	'delightfulcreators:rose_hip_jam_bucket'
-	'delightfulcreators:blazing_chili_bucket'
-	'delightfulcreators:tea_curry_bucket'
+
 })
